0ec789e46c7dd7cfb370ebba5f0655140a44f3fd
Tekyto <Tekyto2014@gmail.com>
2022-11-20 14:44:35 +0200

    js_29_sum-of-string-hard++

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function sumOfString(str) {
-	// write your code here
+	let negative = false;
+	let sum = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		if (!isNaN(Number(str[i]))) {
+			if (negative === true) {
+				sum -= Number(str[i]);
+			} else {
+				sum += Number(str[i]);
+			}
+			negative = false;
+		} else if (str[i] === '-'){
+			negative = true;
+		} else {
+			negative = false;
+		}
+	}
+	return sum;
 }
 
 module.exports = sumOfString;
